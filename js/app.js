82d55680f452504c29c463234bd8f5ee1655ba7d
DianaQuispe <danaalienmas@gmail.com>
2017-08-25 10:23:18 -0500

    d

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /*
  * Archivo principal de JS
  */
+var row = document.getElementById("row");
+var imagenes = ["assets/images/portfolio/cabin.png","assets/images/portfolio/cake.png","assets/images/portfolio/circus.png","assets/images/portfolio/game.png","assets/images/portfolio/safe.png","assets/images/portfolio/submarine.png"];
+for(var i=0; i <imagenes.length; i++) {
+        var div = document.createElement("div");
+    div.setAttribute("class", "col-4");
+    var crearIM = document.createElement("img");
+    crearIM.setAttribute("src", imagenes[i]);
+    crearIM.setAttribute("onclick", 'iMG()');
+    crearIM.style.cursor = "pointer";
+    crearIM.setAttribute("class", "imagenes");
+    div.appendChild(crearIM);
+    row.appendChild(div);
+}
