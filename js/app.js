7527e2da4e2a840f8a5f8a5c98d454a8ed39144f
DianaQuispe <danaalienmas@gmail.com>
2017-08-25 23:32:02 -0500

    d

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
  * Archivo principal de JS
  */
 var row = document.getElementById("row");
-var imagenes = ["assets/images/portfolio/cabin.png","assets/images/portfolio/cake.png","assets/images/portfolio/circus.png","assets/images/portfolio/game.png","assets/images/portfolio/safe.png","assets/images/portfolio/submarine.png"];
-for(var i=0; i <imagenes.length; i++) {
-        var div = document.createElement("div");
+var imagenes = ["assets/images/portfolio/cabin.png", "assets/images/portfolio/cake.png", "assets/images/portfolio/circus.png", "assets/images/portfolio/game.png", "assets/images/portfolio/safe.png", "assets/images/portfolio/submarine.png"];
+for (var i = 0; i < imagenes.length; i++) {
+    var div = document.createElement("div");
     div.setAttribute("class", "col-4");
     var crearIM = document.createElement("img");
     crearIM.setAttribute("src", imagenes[i]);
@@ -14,3 +14,5 @@ for(var i=0; i <imagenes.length; i++) {
     div.appendChild(crearIM);
     row.appendChild(div);
 }
+//////////////////////////
+var header = document.getElementsByTagName("header");
